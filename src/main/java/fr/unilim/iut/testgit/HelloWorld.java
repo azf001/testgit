4c0728790de0b5ebc9118ed03467c177425b487f
Azfar Mehmood <azfar.mehmood98@gmail.com>
2019-04-03 14:16:29 +0200

    fizzbuzz

--- src/main/java/fr/unilim/iut/testgit/HelloWorld.java
+++ src/main/java/fr/unilim/iut/testgit/HelloWorld.java
@@ -2,10 +2,13 @@ package fr.unilim.iut.testgit;
 
 public class HelloWorld {
 	
-	public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-        System.out.println( "Jouons ensemble!" );
-    }
+	 public static void main( String[] args )
+     {
+		System.out.println( "Hello World!" );
+		System.out.println( "Jouons ensemble!" );  
+		
+		System.out.println( "Et si on jouait au FizzBuzz ?" );  
+     System.out.println(FizzBuzz.getResult());
+ }
 
 }
