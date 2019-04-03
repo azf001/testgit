2710d5a67de950e88306d628b8822bfef6199f6c
Azfar Mehmood <azfar.mehmood98@gmail.com>
2019-04-03 14:53:35 +0200

    Ajout Marabout simple 3 mots

--- src/main/java/fr/unilim/iut/testgit/HelloWorld.java
+++ src/main/java/fr/unilim/iut/testgit/HelloWorld.java
@@ -1,17 +1,20 @@
 package fr.unilim.iut.testgit;
 
 public class HelloWorld {
-	
-	 public static void main( String[] args )
-     {
-		System.out.println( "Bonjour tout le monde!" );  
-		System.out.println( "Jouons ensemble!" );  
-		
-		System.out.println( "Et si on jouait au FizzBuzz ?" );  
-     System.out.println(FizzBuzz.getResult());
+			
+		    public static void main( String[] args )
+	            {
+	 			System.out.println( "Bonjour tout le monde!" );  
+	    		System.out.println( "Jouons ensemble!" );  
+				
+				System.out.println( "Et si on jouait au FizzBuzz ?" );  
+	            System.out.println(FizzBuzz.getResult());
 
-		System.out.println( "Et si on jouait au Marabout ?" );  
-     System.out.println(Marabout.getResult());
-     }
+				System.out.println( "Et si on jouait au Marabout ?" );  
+	            System.out.println(Marabout.getResult());
 
+				System.out.println( "Et si on jouait au Marabout de manière plus simple ?" );  
+	            System.out.println(MaraboutSimple.getResult());
+	        }
+	  
 }
