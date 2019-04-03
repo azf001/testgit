3934823fd4398da3644aa8153eef97818f692c7b
Azfar Mehmood <azfar.mehmood98@gmail.com>
2019-04-03 14:54:37 +0200

    Ajout Marabout simple 3 mots de plus

--- src/main/java/fr/unilim/iut/testgit/MaraboutSimple.java
+++ src/main/java/fr/unilim/iut/testgit/MaraboutSimple.java
@@ -1,10 +1,11 @@
 package fr.unilim.iut.testgit;
 	
 
-    public class MaraboutSimple {
+public class MaraboutSimple {
 	
-	    public static String getResult() {
-           return "Marabout, Bout de ficelle, Selle de cheval";
-     }
+    public static String getResult() {
+       return "Marabout, Bout de ficelle, Selle de cheval"
+              + ", Cheval de course, Course à pied, Pied à terre";
+ }
 
-   }   
+}   
