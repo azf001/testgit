310273804873b1b95a0d8f4fa2edc3778049e5d4
Azfar Mehmood <azfar.mehmood98@gmail.com>
2019-04-03 14:44:41 +0200

    marabout

--- src/main/java/fr/unilim/iut/testgit/HelloWorld.java
+++ src/main/java/fr/unilim/iut/testgit/HelloWorld.java
@@ -4,11 +4,17 @@ public class HelloWorld {
 	
 	 public static void main( String[] args )
      {
-		System.out.println( "Hello World!" );
+		System.out.println( "Bonjour tout le monde!" );  
 		System.out.println( "Jouons ensemble!" );  
 		
 		System.out.println( "Et si on jouait au FizzBuzz ?" );  
      System.out.println(FizzBuzz.getResult());
+
+		System.out.println( "Et si on jouait au Marabout ?" );  
+     System.out.println(Marabout.getResult());
+
+		System.out.println( "Et si on jouait au Marabout de manière plus simple ?" );  
+     System.out.println(MaraboutSimple.getResult());
  }
 
 }
